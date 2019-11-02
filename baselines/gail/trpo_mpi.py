19060d56d77eff7071992dbac2e62810e8f1de13
Albert Ge <lbertge@gmail.com>
2019-11-01 18:21:04 -0700

    Move helper functions to util.py
    move rollouts (traj_segemnt_generator), TD estimate (add_vtarg_and_adv),
    flatten_list to utils file

--- baselines/gail/trpo_mpi.py
+++ baselines/gail/trpo_mpi.py
@@ -18,89 +18,7 @@ from baselines.common import colorize
 from baselines.common.mpi_adam import MpiAdam
 from baselines.common.cg import cg
 from baselines.gail.statistics import stats
-
-
-def traj_segment_generator(pi, env, reward_giver, horizon, stochastic):
-
-    # Initialize state variables
-    t = 0
-    ac = env.action_space.sample()
-    new = True
-    rew = 0.0
-    true_rew = 0.0
-    ob = env.reset()
-
-    cur_ep_ret = 0
-    cur_ep_len = 0
-    cur_ep_true_ret = 0
-    ep_true_rets = []
-    ep_rets = []
-    ep_lens = []
-
-    # Initialize history arrays
-    obs = np.array([ob for _ in range(horizon)])
-    true_rews = np.zeros(horizon, 'float32')
-    rews = np.zeros(horizon, 'float32')
-    vpreds = np.zeros(horizon, 'float32')
-    news = np.zeros(horizon, 'int32')
-    acs = np.array([ac for _ in range(horizon)])
-    prevacs = acs.copy()
-
-    while True:
-        prevac = ac
-        ac, vpred = pi.act(stochastic, ob)
-        # Slight weirdness here because we need value function at time T
-        # before returning segment [0, T-1] so we get the correct
-        # terminal value
-        if t > 0 and t % horizon == 0:
-            yield {"ob": obs, "rew": rews, "vpred": vpreds, "new": news,
-                   "ac": acs, "prevac": prevacs, "nextvpred": vpred * (1 - new),
-                   "ep_rets": ep_rets, "ep_lens": ep_lens, "ep_true_rets": ep_true_rets}
-            _, vpred = pi.act(stochastic, ob)
-            # Be careful!!! if you change the downstream algorithm to aggregate
-            # several of these batches, then be sure to do a deepcopy
-            ep_rets = []
-            ep_true_rets = []
-            ep_lens = []
-        i = t % horizon
-        obs[i] = ob
-        vpreds[i] = vpred
-        news[i] = new
-        acs[i] = ac
-        prevacs[i] = prevac
-
-        rew = reward_giver.get_reward(ob, ac)
-        ob, true_rew, new, info = env.step(ac)
-        rews[i] = rew
-        true_rews[i] = true_rew
-
-        cur_ep_ret += rew
-        cur_ep_true_ret += true_rew
-        cur_ep_len += 1
-        if new:
-            ep_rets.append(cur_ep_ret)
-            ep_true_rets.append(cur_ep_true_ret)
-            ep_lens.append(cur_ep_len)
-            cur_ep_ret = 0
-            cur_ep_true_ret = 0
-            cur_ep_len = 0
-            ob = env.reset()
-        t += 1
-
-
-def add_vtarg_and_adv(seg, gamma, lam):
-    new = np.append(seg["new"], 0)  # last element is only used for last vtarg, but we already zeroed it if last new = 1
-    vpred = np.append(seg["vpred"], seg["nextvpred"])
-    T = len(seg["rew"])
-    seg["adv"] = gaelam = np.empty(T, 'float32')
-    rew = seg["rew"]
-    lastgaelam = 0
-    for t in reversed(range(T)):
-        nonterminal = 1-new[t+1]
-        delta = rew[t] + gamma * vpred[t+1] * nonterminal - vpred[t]
-        gaelam[t] = lastgaelam = delta + gamma * lam * nonterminal * lastgaelam
-    seg["tdlamret"] = seg["adv"] + seg["vpred"]
-
+from baselines.gail.utils import traj_segment_generator, add_vtarg_and_adv, flatten_lists
 
 def learn(env, policy_func, reward_giver, expert_dataset, rank,
           pretrained, pretrained_weight, *,
@@ -310,6 +228,7 @@ def learn(env, policy_func, reward_giver, expert_dataset, rank,
         for (lossname, lossval) in zip(loss_names, meanlosses):
             logger.record_tabular(lossname, lossval)
         logger.record_tabular("ev_tdlam_before", explained_variance(vpredbefore, tdlamret))
+
         # ------------------ Update D ------------------
         logger.log("Optimizing Discriminator...")
         logger.log(fmt_row(13, reward_giver.loss_name))
@@ -349,6 +268,4 @@ def learn(env, policy_func, reward_giver, expert_dataset, rank,
         if rank == 0:
             logger.dump_tabular()
 
-
-def flatten_lists(listoflists):
-    return [el for list_ in listoflists for el in list_]
+    return pi
--- baselines/gail/utils.py
+++ baselines/gail/utils.py
@@ -1,21 +1,17 @@
 import numpy as np
 
 def traj_segment_generator(pi, env, reward_giver, horizon, stochastic):
-
-    # Initialize state variables
     t = 0
-    ac = env.action_space.sample()
-    new = True
-    rew = 0.0
-    true_rew = 0.0
+    ac = env.action_space.sample() # not used, just so we have the datatype
+    new = True # marks if we're on first timestep of an episode
     ob = env.reset()
 
-    cur_ep_ret = 0
-    cur_ep_len = 0
+    cur_ep_ret = 0 # return in current episode
     cur_ep_true_ret = 0
+    cur_ep_len = 0 # len of current episode
     ep_true_rets = []
-    ep_rets = []
-    ep_lens = []
+    ep_rets = [] # returns of completed episodes in this segment
+    ep_lens = [] # lengths of ...
 
     # Initialize history arrays
     obs = np.array([ob for _ in range(horizon)])
@@ -33,10 +29,9 @@ def traj_segment_generator(pi, env, reward_giver, horizon, stochastic):
         # before returning segment [0, T-1] so we get the correct
         # terminal value
         if t > 0 and t % horizon == 0:
-            yield {"ob": obs, "rew": rews, "vpred": vpreds, "new": news,
-                   "ac": acs, "prevac": prevacs, "nextvpred": vpred * (1 - new),
-                   "ep_rets": ep_rets, "ep_lens": ep_lens, "ep_true_rets": ep_true_rets}
-            _, vpred = pi.act(stochastic, ob)
+            yield {"ob" : obs, "rew" : rews, "vpred" : vpreds, "new" : news,
+                    "ac" : acs, "prevac" : prevacs, "nextvpred": vpred * (1 - new),
+                    "ep_rets" : ep_rets, "ep_lens" : ep_lens, "ep_true_rets": ep_true_rets}
             # Be careful!!! if you change the downstream algorithm to aggregate
             # several of these batches, then be sure to do a deepcopy
             ep_rets = []
@@ -51,6 +46,8 @@ def traj_segment_generator(pi, env, reward_giver, horizon, stochastic):
 
         rew = reward_giver.get_reward(ob, ac)
         ob, true_rew, new, _ = env.step(ac)
+        # ob, rew, new, _ = env.step(ac)
+        # true_rew = rew
         rews[i] = rew
         true_rews[i] = true_rew
 
@@ -67,9 +64,11 @@ def traj_segment_generator(pi, env, reward_giver, horizon, stochastic):
             ob = env.reset()
         t += 1
 
-
 def add_vtarg_and_adv(seg, gamma, lam):
-    new = np.append(seg["new"], 0)  # last element is only used for last vtarg, but we already zeroed it if last new = 1
+    """
+    Compute target value using TD(lambda) estimator, and advantage with GAE(lambda)
+    """
+    new = np.append(seg["new"], 0) # last element is only used for last vtarg, but we already zeroed it if last new = 1
     vpred = np.append(seg["vpred"], seg["nextvpred"])
     T = len(seg["rew"])
     seg["adv"] = gaelam = np.empty(T, 'float32')
@@ -80,3 +79,6 @@ def add_vtarg_and_adv(seg, gamma, lam):
         delta = rew[t] + gamma * vpred[t+1] * nonterminal - vpred[t]
         gaelam[t] = lastgaelam = delta + gamma * lam * nonterminal * lastgaelam
     seg["tdlamret"] = seg["adv"] + seg["vpred"]
+
+def flatten_lists(listoflists):
+    return [el for list_ in listoflists for el in list_]
